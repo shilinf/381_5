32dceea23fe492c800ea3bf02bdc9327a784a86e
shilinf <shilinf@umich.edu>
2014-03-31 17:25:46 -0400

    compile

--- Cruise_ship.cpp
+++ Cruise_ship.cpp
@@ -3,10 +3,13 @@
 #include "Island.h"
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 
 using std::cout; using std::endl;
 using std::shared_ptr;
 using std::vector;
+using std::find_if;
 
 Cruise_ship::Cruise_ship(const std::string& name_, Point position_) : Ship(name_, position_, 500., 15., 2., 0), cruise_state(NO_DESTINATION) {}
 
